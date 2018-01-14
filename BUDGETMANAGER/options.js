2d9865275c38685a100f0163d0363ccc654d232a
rajathacker <kshitijpratap0809@gmail.com>
2018-01-14 23:50:54 +0530

    notifications added

--- BUDGETMANAGER/options.js
+++ BUDGETMANAGER/options.js
@@ -1,4 +1,8 @@
 $(function(){
+chrome.storage.sync.get('limit',function(budget){
+	$('#limit').val(budget.limit);
+})
+
 	$('#saveLimit').click(function(){
 		var limit=$('#limit').val();
         if(limit){
@@ -8,6 +12,14 @@ $(function(){
         }
 	});
 	$('#resetTotal').click(function(){
-		chrome.storage.sync.set({'total':0});
+		chrome.storage.sync.set({'total':0},function(){
+			var notifOptions={
+        			type:'basic',
+        			iconUrl:'icon48.png',
+        			title:'total reset',
+        			message:"total has been reset to 0"
+        		};
+            chrome.notifications.create('limitNotif',notifOptions);
+		});
 	});
 });
--- BUDGETMANAGER/popup.js
+++ BUDGETMANAGER/popup.js
@@ -1,6 +1,12 @@
 $(function(){
+    chrome.storage.sync.get(['total','limit'],function(budget){
+    	$('#total').text(budget.total);
+    	$('#limit').text(budget.limit);
+    })
+
+
 	$('#spendAmount').click(function(){	
-		chrome.storage.sync.get('total',function(budget){
+		chrome.storage.sync.get(['total','limit'],function(budget){
            var newtotal=0;
            if(budget.total)
            {
@@ -13,7 +19,17 @@ $(function(){
              newtotal+=parseInt(amount);
            }
 
-        chrome.storage.sync.set({'total':newtotal});
+        chrome.storage.sync.set({'total':newtotal},function(){
+        	if (amount && total>budget.limit) {
+        		var notifOptions={
+        			type:'basic',
+        			iconUrl:'icon48.png',
+        			title:'limit reached',
+        			message:"uh! you have reached the limit"
+        		};
+        		chrome.notifications.create('limitNotif',notifOptions);
+        	}
+        });
         
         $('#total').text(newtotal);
         $('#amount').val('');   
